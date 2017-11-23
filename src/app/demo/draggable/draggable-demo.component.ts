94a1567b0b935d3e8f15ceb27895d3c8e94f8213
qxiao <qxiao@thoughtworks.com>
2017-11-23 17:41:11 +0800

    更改drag样式

--- src/app/demo/draggable/draggable-demo.component.ts
+++ src/app/demo/draggable/draggable-demo.component.ts
@@ -1,19 +1,55 @@
-import { Component } from '@angular/core';
-import { DraggableDirective } from '../../exports';
+import {Component, OnInit} from '@angular/core';
+import {DraggableDirective} from '../../exports';
+import {Source} from './draggable-demo.model';
 
 @Component({
   selector: 're-draggable-demo',
   templateUrl: './draggable-demo.component.html'
 })
-export class DraggableDemoComponent {
-  source = new Array(10).fill(0).map((_, index) => index + 1);
+export class DraggableDemoComponent implements OnInit {
+  source: Source[];
   target = [];
 
+  ngOnInit(): void {
+    this.source = [
+      {
+        name: 'xx',
+        id: 1,
+      }, {
+        name: 'aa',
+        id: 2,
+      }, {
+        name: 'cc',
+        id: 3,
+      }, {
+        name: 'gg',
+        id: 121,
+      }, {
+        name: 'hh',
+        id: 213,
+      }, {
+        name: 'jj',
+        id: 313,
+      },
+    ]
+  }
+
   onDrop2Right($event) {
-    const contextData = $event.dataTransfer.getData(DraggableDirective.DRAGGABLE_DATA_KEY);
-    const data = JSON.parse(contextData);
-    console.log('drag data to right', data);
-    this.source = this.source.filter(item => item !== data.data);
-    this.target = [...this.target, data.data];
+    const targetValue = $event.toElement.innerText;
+    const newIndex = this.findIndex(targetValue);
+    const data = $event.dataTransfer.getData(DraggableDirective.DRAGGABLE_DATA_KEY);
+    const fromValue = JSON.parse(data).data.name;
+    const oldIndex = this.findIndex(fromValue);
+    this.arrayMove(oldIndex, newIndex);
+  }
+
+  findIndex(targetName: string) {
+    return this.source.map(s => s.name).indexOf(targetName);
+  }
+
+  arrayMove(oldIndex, newIndex) {
+    const element = this.source[oldIndex];
+    this.source.splice(oldIndex, 1);
+    this.source.splice(newIndex, 0, element);
   }
 }
---
+++ src/app/demo/draggable/draggable-demo.model.ts
@@ -0,0 +1,5 @@
+export interface Source {
+  name?: string;
+  id?: number;
+  des?: string;
+}
